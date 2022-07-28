751098ed822df007c48176dba22bdf53623aa41d
Yana <bondarevayana@gmail.com>
2022-07-28 19:22:23 +0300

    Main and README added

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args){
+    SalesManager manager1 = new SalesManager (new int[]{2, 3, 5});
+
+System.out.println(manager1.max());
+    }
+}
